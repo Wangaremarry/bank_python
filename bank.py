2ec1fd17cfdc9a48985b4db13fdf4d8504074a8a
wangare <marywangareee@gmail.com>
2022-06-10 22:01:59 +0300

    account

--- bank.py
+++ bank.py
@@ -13,22 +13,30 @@ class Account:
         else:
             self.balance += amount
             self.deposits.append(amount)
-            return f"You have deposited {amount}. Your new balance is {self.balance}"  
+            return f"You have deposited {amount},your new balance is {self.balance} and your deposits are {self.deposits}."  
 
     def withdraw(self,amount):
+        self.trasaction=100
         if amount>self.balance:
-             return f"Your balance is {self.balance}, you cannot withdraw the {amount}" 
+             return f"Your balance is {self.balance},so you cannot withdraw the {amount}." 
         elif amount<=0:
             return f"Amount must be greater than zero" 
         else:
-            self.balance-=amount
+            self.balance-=amount+self.trasaction
             self.withdrawals.append(amount)
-            return f"you have withdraw {amount} your balance is {self.balance}" 
+            return f"You have withdraw {amount},your new balance is {self.balance} and your withdrawals are{ self.withdrawals}." 
     
 
     def deposits_statement(self):
-
-        print(*self.deposits, sep="\n")
+        for statement in self.deposits:
+         print(statement, sep="\n")
 
     def withdrawals_statement(self):
-        print(*self.withdrawals, sep="\n")
+        for v in self.withdrawals:
+         print(v, sep="\n")
+        
+    def current_balance(self):
+        return self.balance
+        
+    
+        
